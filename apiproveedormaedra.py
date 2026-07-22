9ceaf94d1b6d8b47d1b4167984f12882c87352a0
Lucia Masis Quesada <89036648+est26@users.noreply.github.com>
2026-07-22 11:50:07 -0600

    cambios

--- apiproveedormaedra.py
+++ apiproveedormaedra.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="API Proveedor de Madera (Simulada)")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 inventario_madera = {
-    "cedro": {"precio_m3": 185000, "disponible": True, "stock_m3": 42, "tiempo_entrega_dias": 5},
-    "pino":  {"precio_m3": 95000,  "disponible": True, "stock_m3": 78, "tiempo_entrega_dias": 3},
-    "roble": {"precio_m3": 260000, "disponible": True, "stock_m3": 15, "tiempo_entrega_dias": 10},
-    "caoba": {"precio_m3": 310000, "disponible": False, "stock_m3": 0, "tiempo_entrega_dias": None},
+    "cipres":  {"precio_m3": 95000,  "disponible": True,  "stock_m3": 60, "tiempo_entrega_dias": 3},
+    "laurel":  {"precio_m3": 130000, "disponible": True,  "stock_m3": 40, "tiempo_entrega_dias": 4},
+    "cedro":   {"precio_m3": 185000, "disponible": True,  "stock_m3": 42, "tiempo_entrega_dias": 5},
+    "teca":    {"precio_m3": 260000, "disponible": True,  "stock_m3": 15, "tiempo_entrega_dias": 10},
+    "pochote": {"precio_m3": 310000, "disponible": False, "stock_m3": 0,  "tiempo_entrega_dias": None},
 }
 
 @app.get("/proveedor/madera")
